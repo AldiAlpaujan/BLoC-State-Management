307a4df523d408fde8f8b44b0acf3d744cf79e24
AldiAlpaujan <aldi.m.alpaujan.2@gmail.com>
2021-12-06 19:36:53 +0700

    menambahkan file color_bloc.dart dan membuat bLoC

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_bloc_package/color_bloc.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,35 +12,47 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        floatingActionButton: Row(
-          mainAxisAlignment: MainAxisAlignment.end,
-          children: [
-            FloatingActionButton(
-              onPressed: () {},
-              backgroundColor: Colors.amber,
-            ),
-            const SizedBox(
-              width: 10,
-            ),
-            FloatingActionButton(
-              onPressed: () {},
-              backgroundColor: Colors.lightBlue,
-            )
-          ],
-        ),
-        appBar: AppBar(
-          title: const Center(
-            child: Text("Flutter BLoC packAge"),
+      home: BlocProvider<ColorBLoC>(
+        builder: (context) => ColorBLoC(),
+        child: const MainPage(),
+      ),
+    );
+  }
+}
+
+class MainPage extends StatelessWidget {
+  const MainPage({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      floatingActionButton: Row(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          FloatingActionButton(
+            onPressed: () {},
+            backgroundColor: Colors.amber,
           ),
+          const SizedBox(
+            width: 10,
+          ),
+          FloatingActionButton(
+            onPressed: () {},
+            backgroundColor: Colors.lightBlue,
+          )
+        ],
+      ),
+      appBar: AppBar(
+        title: const Center(
+          child: Text("Flutter BLoC packAge"),
         ),
-        body: Center(
-          child: AnimatedContainer(
-              duration: const Duration(milliseconds: 500),
-              width: 100,
-              height: 100,
-              color: Colors.amber),
-        ),
+      ),
+      body: Center(
+        child: AnimatedContainer(
+            duration: const Duration(milliseconds: 500),
+            width: 100,
+            height: 100,
+            color: Colors.amber),
       ),
     );
   }
